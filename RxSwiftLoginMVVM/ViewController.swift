b9ef3c35e2de59e89a2d72b95f2f114d03da17fa
Gael Robin <gael.robin@gmail.com>
2018-04-26 22:18:53 +0200

    Starter project

--- RxSwiftLoginMVVM/ViewController.swift
+++ RxSwiftLoginMVVM/ViewController.swift
@@ -10,16 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
